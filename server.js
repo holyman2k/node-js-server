7fcc83878cbbcc0bb9c2de1e4927ae70018db007
Charlie Wu <charlie_y_wu@hotmail.com>
2012-02-17 15:48:33 +1100

    initial check in

---
+++ server.js
@@ -0,0 +1,54 @@
+var sys = require("util"),
+    http = require("http"),
+	url = require("url"),
+	path = require("path"),
+	fs = require("fs"),
+	mime = require("mime");
+	
+function onRequest(request, response) {
+		
+		var uri = url.parse(request.url).pathname;
+		
+		var filename = path.join(process.cwd(), uri);
+			
+		filename = uri == "/" ? path.join(process.cwd(), "index.html") : filename;
+		
+		path.exists(filename, function(exists){
+		
+			var mimeType = mime.lookup(filename);
+			
+			console.log("file path: " + filename + ", mime type: " + mimeType);
+			
+			if (!exists) {
+			
+				response.writeHead(404, {"Content-Type": "text/plain"});
+				response.end("404 Not Found \n");
+				return;
+			}
+			
+			fs.readFile(filename, "binary", function(err, file) {
+			
+				if (err) {
+					response.writeHead(500, {"Content-Type": "text/plain"});
+					response.end(err + "\n");
+					return;
+				}
+				response.writeHead(200, {"Content-Type": mimeType}); //response.writeHead(200);
+				
+				response.end(file, "binary");
+			});
+		});
+	}
+
+function start() {
+		
+	var port = process.env.C9_PORT;
+	port = port === undefined ? "80" : port;
+
+	var server = http.createServer(onRequest);
+	server.listen(port);
+
+	console.log("Server running at http://localhost:" + port + "/");
+}
+
+exports.start = start;
